72921026abaa49d40f6ecb67d6e06b3f609ec4a0
MarcosTholken <Marcos.Tholken@hotmail.com>
2015-04-26 18:58:08 -0300

    Update Pratica32.java

--- src/main/java/Pratica32.java
+++ src/main/java/Pratica32.java
@@ -11,9 +11,16 @@ import static java.lang.Math.*;
  * @author Marcos
  */
 public class Pratica32 {
+    public static void main(String[] args) {
+        /* Dentro do método main faça uma chamada para a função densidade, 
+        passando valores para x, media e desvio de -1, 67 e 3 respectivamente. 
+        Exiba o valor calculado. Verifique se o valor calculado está correto.*/
+        System.out.println(densidade(-1, 67, 3));
+    }
     
-    public static double densidade(double x, double media, double desvio) {
+    public static double densidade(double x, double media, 
+            double desvio) {
         double d = (exp(-0.5*(pow((x - media)/desvio, 2))))/(sqrt(2*PI)*desvio);
         return d;
-    }    
+    }
 }
